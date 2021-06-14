48952e4296e70af3400e964979f88a362f70f490
pellehardstedt <pelle.hardstedt>
2021-06-14 13:08:58 +0200

    first

---
+++ src/test/java/com/example/oop_avancerad_3/OopAvancerad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oop_avancerad_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopAvancerad3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
